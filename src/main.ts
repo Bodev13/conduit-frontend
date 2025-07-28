8dad7b474af7ff7f12a6a0954e4aa9fbd3cc2963
Bodev13 <bo.sadriddin@gmail.com>
2025-07-28 15:27:57 +0200

    Changed environment.prod.ts from hardcoded localhost to empty string, also changed main.ts from hardcoded to variables.

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,9 @@ import { AppModule } from "./app/app.module";
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
+
+import { environment } from "./environments/environment";
+
+const hostname = window.location.hostname;
+const apiPort = 8000;
+environment.API_URL = `http://${hostname}:${apiPort}/api`;
